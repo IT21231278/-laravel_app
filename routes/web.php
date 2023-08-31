346fe9beedbae0e29f55e0541e1345382ab4edcb
K.Rashmika Lankari Perera <rashmilankari19@gmail.com>
2023-08-31 13:17:37 +0530

    updated use App\Http\Controllers\ProductController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
  
 Route::get('/', function () {
     return view('welcome');
